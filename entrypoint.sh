c3f1d8f4946504b63cc5fe2e36b243ef04e430e7
mrnitr0 <51184732+mrnitr0@users.noreply.github.com>
2021-07-08 07:06:17 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 rm start.sh
-curl -Lo ./start.sh https://gitlab.com/samcozza03/pterodactyl-vps-egg/raw/main/start.sh
+curl -Lo ./start.sh https://raw.githubusercontent.com/mrnitr0/Pterodactyl-VPS-Egg/main/start.sh
 chmod +x ./start.sh
 # Run the Server
 bash ./start.sh
