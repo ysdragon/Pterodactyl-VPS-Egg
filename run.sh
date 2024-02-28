85122ffc1b1507e5a732442a067878bf1599fea6
ysdragon <youssefelkholey@gmail.com>
2024-02-28 05:03:36 +0200

    Add run.sh

---
+++ run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+clear
+  echo -e "\e[32m╭────────────────────────────────────────────────────────────────────────────────╮\e[0m"
+  echo -e "\e[32m│                                                                                │\e[0m"
+  echo -e "\e[32m│                             Pterodactyl VPS EGG                                │\e[0m"
+  echo -e "\e[32m│                                                                                │\e[0m"
+  echo -e "\e[32m│                           © 2021 - 2024 ysdragon                               │\e[0m"
+  echo -e "\e[32m│                                                                                │\e[0m"
+  echo -e "\e[32m╰────────────────────────────────────────────────────────────────────────────────╯\e[0m"
+  echo "                                                                                                "
+  echo "root@MyVPS:~                                                                                    "
+
+run_cmd() {
+    read -p "root@MyVPS:~ " CMD
+    eval "$CMD"
+    echo "root@MyVPS:~ "
+    run_user_cmd
+}
+
+run_user_cmd() {
+    read -p "user@MyVPS:~ " CMD2
+    eval "$CMD2"
+    echo "root@MyVPS:~ "
+    run_cmd
+}
+
+run_cmd
