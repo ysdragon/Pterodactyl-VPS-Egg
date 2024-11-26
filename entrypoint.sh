e4660731d7b18e8946aa69e47ee1ef270f9c4476
ysdragon <youssefelkholey@gmail.com>
2024-11-26 02:31:30 +0200

    Add the startup helper script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+# Sleep for 2 seconds to ensure container is ready
+sleep 2
+
 cd /home/container
 MODIFIED_STARTUP=$(eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
 
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=$(ip route get 1 | awk '{print $NF;exit}')
 
-# Copy run.sh to /home/container
-cp /run.sh "$HOME/run.sh"
+# Check if already installed
+if [ ! -e "$HOME/.installed" ]; then
+    /usr/local/bin/proot \
+    --rootfs="/" \
+    -0 -w "/root" \
+    -b /dev -b /sys -b /proc -b /etc/resolv.conf \
+    --kill-on-exit \
+    /bin/bash "/install.sh" || exit 1
+fi
 
-# Run the VPS Installer
-bash /install.sh
+# Run the startup helper script
+bash /helper.sh
---
+++ helper.sh
@@ -0,0 +1,34 @@
+# Parse port configuration
+parse_ports() {
+    local config_file="$HOME/vps.config"
+    local port_args=""
+    
+    while read -r line; do
+        case "$line" in
+            internalip=*) ;;
+            port[0-9]*=*)
+                port=${line#*=}
+                if [ -n "$port" ]; then port_args=" -p $port:$port$port_args"; fi
+            ;;
+            port=*)
+                port=${line#*=}
+                if [ -n "$port" ]; then port_args=" -p $port:$port$port_args"; fi
+            ;;
+        esac
+    done <"$config_file"
+}
+
+# Execute PRoot environment
+exec_proot() {
+    local port_args=$(parse_ports)
+    
+    /usr/local/bin/proot \
+    --rootfs="${HOME}" \
+    -0 -w "${HOME}" \
+    -b /dev -b /sys -b /proc -b /etc/resolv.conf \
+    $port_args \
+    --kill-on-exit \
+    /bin/sh "/run.sh"
+}
+
+exec_proot
